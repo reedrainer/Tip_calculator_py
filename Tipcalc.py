c4687429c4c4a1f08400a2c0a85d4152b5109006
reedrainer <jamesdrreed@gmail.com>
2021-03-29 15:44:53 +0100

    Add files via upload

---
+++ Tipcalc.py
@@ -0,0 +1,17 @@
+print("James' really cool tip calculator")
+full_amount = input("Pleae input the bill total here!\n£")
+
+people = input("How many people are splitting the bill?\n")
+
+tip_percent = input("Enter percentile tip as a 0.# number (e.g 0.15 for 15%)\n")
+15
+
+tip_for_calc = float(tip_percent)
+
+tip = int(full_amount) * tip_for_calc
+
+final_calc = round((int(full_amount) + tip) / int(people),2)
+
+print(f"Ok so each person needs to pay\n£{final_calc}")
+
+input("anything else?")
